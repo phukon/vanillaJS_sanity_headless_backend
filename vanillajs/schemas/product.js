d35ea440ef3c73030bfe884167006a7a39657240
phukon <rikiphukon16@gmail.com>
2023-01-28 05:18:40 +0530

    initial commit

---
+++ vanillajs/schemas/product.js
@@ -0,0 +1,23 @@
+export default {
+    name: 'product',
+    title: 'Product',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string'
+      },
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'text'
+      },
+    ],
+    preview: {
+      select: {
+        title: 'title',
+      }
+    }
+  }
+  
